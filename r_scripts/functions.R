93c45c41ef705c983d2e9e99cd8ce63094954742
Andrew Flowers <andrew.w.flowers@gmail.com>
2016-03-05 18:38:58 -0500

    update scripts and instructions, add latest comparisons

--- r_scripts/functions.R
+++ r_scripts/functions.R
@@ -8,13 +8,14 @@ require(dplyr)
 
 clean_country_string <- function(country_column){
   
+  country_column <- str_extract(country_column, pattern='[^-]+$')
+  
   country_column <- gsub(".", "", country_column,fixed=TRUE) 
   country_column <- gsub("/", "_", country_column, fixed=TRUE)
   country_column <- gsub("-", "_", country_column, fixed=TRUE) 
   country_column <- gsub("(", "", country_column, fixed=TRUE)
   country_column <- gsub(")", "", country_column, fixed=TRUE)
   
-  country_column <- str_extract(country_column, pattern='[^-]+$')
   country_column <- str_trim(country_column)
   
   return(country_column)
